97694ef0a522ae1b25d3a48bf8840f7b79cdacd6
Baozisoftware <hlj493595708@hotmail.com>
2017-03-25 13:59:50 +0800

    Add files via upload

--- api/getters/quanmin.go
+++ api/getters/quanmin.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"strconv"
 )
 
 //quanmin 全民直播
@@ -28,7 +29,7 @@ func (i *quanmin) GetRoomInfo(url string) (id string, live bool, err error) {
 	url = strings.ToLower(url)
 	reg, _ := regexp.Compile("quanmin\\.tv/(\\w+)")
 	id = reg.FindStringSubmatch(url)[1]
-	url = fmt.Sprintf("http://www.quanmin.tv/json/rooms/%s/info1.json", id)
+	url = fmt.Sprintf("http://www.quanmin.tv/json/rooms/%s/noinfo.json", id)
 	tmp, err := httpGet(url)
 	json := *(pruseJSON(tmp))
 	live = json["play_status"].(bool)
@@ -46,13 +47,14 @@ func (i *quanmin) GetLiveInfo(id string) (live LiveInfo, err error) {
 		}
 	}()
 	live = LiveInfo{RoomID: id}
-	url := fmt.Sprintf("http://www.quanmin.tv/json/rooms/%s/info1.json", id)
+	url := fmt.Sprintf("http://www.quanmin.tv/json/rooms/%s/noinfo.json", id)
 	tmp, err := httpGet(url)
 	json := *pruseJSON(tmp)
 	nick := json["nick"].(string)
 	title := json["title"].(string)
 	details := json["intro"].(string)
 	img := json["thumb"].(string)
+	id=strconv.FormatFloat(json["uid"].(float64),'f',0,64)
 	video := fmt.Sprintf("http://flv.quanmin.tv/live/%s.flv", id)
 	live.LiveNick = nick
 	live.LivingIMG = img
