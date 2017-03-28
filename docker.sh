3b085ae7414055d730b846421b677d0c42873477
Baozisoftware <->
2017-03-28 18:16:07 +0800

    docker update

---
+++ docker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -H "Content-Type: application/json" --data '{"docker_tag": "$DOCKER_TAG"}' -X POST https://registry.hub.docker.com/u/$DOCKER_USER/$DOCKER_REPO/trigger/$DOCKER_TRIGGER_TOKEN/
